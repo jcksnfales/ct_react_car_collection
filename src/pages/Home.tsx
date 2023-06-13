83b12b95f27580046e41c6c57cae596dfbab71bd
jcksnfales <jcksnfales@gmail.com>
2023-06-13 16:45:44 -0400

    Added Home page content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="mt-9">
+            <h1 className="mx-auto w-fit font-bold text-3xl text-slate-800 track">Catalogue cars with us!</h1>
+            <p className="mx-auto w-fit mt-4">Give us some info about cars!</p>
+            <p className="mx-auto w-fit mt-4 font-light italic">Sign in or go to Dashboard to get started</p>
         </div>
     )
 }
